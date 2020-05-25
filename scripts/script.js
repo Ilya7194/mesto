020479bc3d802b9bb50dd04525008d170ed33fe8
Ilya Sedymov <sedymovilya@mail.ru>
2020-05-25 15:16:12 +0300

    оптимизирован код в файле scripts.js

--- scripts/script.js
+++ scripts/script.js
@@ -27,22 +27,10 @@ const initialCards = [
 ];
 
 initialCards.forEach(element => {
-  name = element.name;
-  link = element.link;
-  alt = element.name;
+  const name = element.name;
+  const link = element.link;
   addCard(link, name);
 });
-
-// showCard();
-
-// function showCard() {
-//   for (let i = 0; i < initialCards.length; i++) {
-//     name = initialCards[i].name;
-//     link = initialCards[i].link;
-//     alt = initialCards[i].name;
-//     addCard(link, name);
-//   }
-// }
 
 //Редактирование профиля___________________________________________________________________________________
 const openProfileFormButton = document.querySelector('.profile__edit-button');
@@ -50,15 +38,15 @@ const closeProfileFormButton = document.querySelector('.popup__close-button_plac
 const popupProfle = document.querySelector('.popup_profile');
 const saveButton = document.querySelector('.popup__submit-button_save');
 
-let profileName = document.querySelector('.profile__name');
-let profileVacation = document.querySelector('.profile__vocation');
-let nameInput = document.querySelector('.popup__input_edit_name');
-let jobInput = document.querySelector('.popup__input_edit_about');
-let formEditProfile = document.querySelector('.popup__container_place_profile');
+const profileName = document.querySelector('.profile__name');
+const profileVacation = document.querySelector('.profile__vocation');
+const nameInput = document.querySelector('.popup__input_edit_name');
+const jobInput = document.querySelector('.popup__input_edit_about');
+const formEditProfile = document.querySelector('.popup__container_place_profile');
 
 // Открытие/закрытие попапа редактирования профиля____________________________________________________
 function toggleProfileForm() {
-  toggleClass(popupProfle);;
+  popupProfle.classList.toggle('popup_opened');
   if (popupProfle.classList.contains('popup_opened')) {
     nameInput.value = profileName.textContent;
     jobInput.value = profileVacation.textContent;
@@ -81,22 +69,22 @@ const openCardFormButton = document.querySelector('.profile__add-button');
 const closeCardFormButton = document.querySelector('.popup__close-button_place_card');
 const popupCard = document.querySelector('.popup_card')
 const createButton = document.querySelector('.popup__submit-button_create');
-let formAddCard = document.querySelector('.popup__container_place_card');
+const formAddCard = document.querySelector('.popup__container_place_card');
 
 function addCard(link, name) { // функция создания карточки
   const cardTemplate = document.querySelector('#card-template').content;
   cardElement = cardTemplate.cloneNode(true);
   cardElement.querySelector('.place__image').src = link;
   cardElement.querySelector('.place__title').textContent = name;
-  cardElement.querySelector('.place__image').alt = alt;
+  cardElement.querySelector('.place__image').alt = name;
   cardElement.querySelector('.place__delete-button').addEventListener('click', function (evt) { //удаление карточки
-    evt.target.parentElement.remove();
+    this.parentElement.remove();
   });
   cardElement.querySelector('.place__like-button').addEventListener('click', function (evt) { // Лайк карточки
     evt.target.classList.toggle('place__like-button_active')
   });
   cardElement.querySelector('.place__image').addEventListener('click', function (evt) { //откытие увеличенной карточки
-    popupPhoto.classList.add('popup-image_opened');
+    popupPhoto.classList.add('popup_opened');
     popupPhotoImage.src = link;
     popupPhotoImage.alt = name;
     popupPhotoCaption.textContent = name;
@@ -105,8 +93,8 @@ function addCard(link, name) { // функция создания карточк
 }
 
 // Открытие/закрытие попапа добавления карочки_______________________________________________________________________
-function toggleCardForm() { 
-  toggleClass(popupCard);
+function toggleCardForm() {
+ popupCard.classList.toggle('popup_opened');
 }
 
 openCardFormButton.addEventListener('click', toggleCardForm);
@@ -114,11 +102,10 @@ closeCardFormButton.addEventListener('click', toggleCardForm);
 formAddCard.addEventListener('submit', addFormSubmitHandler);
 
 function addFormSubmitHandler(evt) { // обработчик попапа добавления карточки
-  let imageInput = document.querySelector('.popup__input_add_image');
-  let placeInput = document.querySelector('.popup__input_add_place');
-  name = placeInput.value;
-  link = imageInput.value;
-  alt = placeInput.value;
+  const imageInput = document.querySelector('.popup__input_add_image');
+  const placeInput = document.querySelector('.popup__input_add_place');
+  const name = placeInput.value;
+  const link = imageInput.value;
   evt.preventDefault();
   toggleCardForm();
   addCard(link, name);
@@ -126,16 +113,12 @@ function addFormSubmitHandler(evt) { // обработчик попапа доб
   imageInput.value = '';
 }
 // Закрытие попапа увеличения карточки. (открытие попапа реализовано в функции создания карточки)_____________________
-const closePhotoPopupButton = document.querySelector('.popup-image__close-button');
-const popupPhoto = document.querySelector('.popup-image');
-const popupPhotoImage = document.querySelector('.popup-image__photo');
-const popupPhotoCaption = document.querySelector('.popup-image__caption');
+const closePhotoPopupButton = document.querySelector('.popup__close-button_place_image');
+const popupPhoto = document.querySelector('.popup_image');
+const popupPhotoImage = popupPhoto.querySelector('.popup__photo');
+const popupPhotoCaption = popupPhoto.querySelector('.popup__caption');
 
-function closePhotoPopup() { 
-  popupPhoto.classList.remove('popup-image_opened');
+function closePhotoPopup() {
+  popupPhoto.classList.remove('popup_opened');
 }
 closePhotoPopupButton.addEventListener('click', closePhotoPopup);
-
-function toggleClass(elem) { // функция открытия закрытия попапов добавления карточки и редактирования профиля
-  elem.classList.toggle('popup_opened');
-}
